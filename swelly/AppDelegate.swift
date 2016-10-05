afa9ea6de52ffe8f92434564c86ea5c1891356b0
Zhang Chen <tgfbeta@me.com>
2016-10-05 10:22:57 +0800

    Initial Commit

---
+++ swelly/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  swelly
+//
+//  Created by ZhangChen on 05/10/2016.
+//
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ swelly/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  swelly
+//
+//  Created by ZhangChen on 05/10/2016.
+//
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
